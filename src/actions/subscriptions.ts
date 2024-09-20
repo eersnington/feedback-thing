4ec35e94d6261c5143dcbebccd6dcdb56070b9f1
Sree Narayanan <sreeaadhi07@gmail.com>
2024-09-20 18:15:48 +0400

    Update subscriptions.ts

--- src/actions/subscriptions.ts
+++ src/actions/subscriptions.ts
@@ -5,11 +5,25 @@ import { db } from "@/server/db";
 import { users } from "@/server/db/schema";
 import { eq } from "drizzle-orm";
 import { currentUser } from "@clerk/nextjs/server";
+import {
+  getUserFeedbackCount,
+  getUserProjectCount,
+  isUserPaid,
+} from "@/server/quota";
 
 export interface SubscriptionData {
   subscriptionId: string | null;
   plan: "Free" | "Pro";
   subscriptionNextBilledAt: Date | null;
+}
+
+export interface UsageData {
+  feedbackItems: number;
+  feedbackLimit: string;
+  projects: number;
+  projectLimit: string;
+  message: string;
+  isPaid: boolean;
 }
 
 export async function fetchSubscriptionData(): Promise<SubscriptionData> {
@@ -46,5 +60,39 @@ export async function fetchSubscriptionData(): Promise<SubscriptionData> {
     subscriptionId: userData.subscriptionId,
     plan: userData.subscriptionId ? "Pro" : "Free",
     subscriptionNextBilledAt: userData.subscriptionNextBilledAt,
+  };
+}
+
+export async function fetchUsageData(): Promise<UsageData> {
+  const userAuth = await currentUser();
+
+  if (!userAuth) {
+    throw new Error("Unauthorized");
+  }
+
+  const userProjectCount = await getUserProjectCount(userAuth.id);
+
+  const userFeedbackCount = await getUserFeedbackCount(userAuth.id);
+
+  const userPaidBool = await isUserPaid(userAuth.id);
+
+  if (userPaidBool) {
+    return {
+      feedbackItems: userFeedbackCount,
+      feedbackLimit: "unlimited",
+      projects: userProjectCount,
+      projectLimit: "unlimited",
+      message: "Thank you for being a Pro user!",
+      isPaid: true,
+    };
+  }
+
+  return {
+    feedbackItems: userFeedbackCount,
+    feedbackLimit: "50",
+    projects: userProjectCount,
+    projectLimit: "1",
+    message: "Upgrade to access more projects and feedback",
+    isPaid: false,
   };
 }
