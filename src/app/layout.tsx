de18a7c0efd57ec7fefeb7daee893507c7f68e77
Sree Narayanan <sreeaadhi07@gmail.com>
2024-09-22 03:09:42 +0400

    updates to og and hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,45 @@ import { PHProvider } from "@/components/posthog/provider";
 export const metadata: Metadata = {
   title: "FeedbackThing",
   description: "Your only tool needed to get Feedback and Bug Reports",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  creator: "@Sreenington",
+  keywords: [
+    "Feedback",
+    "Bug Reports",
+    "FeedbackThing",
+    "FeedbackThing Pro",
+    "FeedbackThing Pro",
+    "Feedback screenshot tool",
+    "Feedback screenshot",
+    "Feedback tool",
+    "Feedback collection saas",
+    "Feedback collection widget",
+    "features.vote",
+    "Features voting app",
+    "Features voting producthunt",
+  ],
+
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://feedbackthing.pro",
+    title: "FeedbackThing",
+    description: "Your only tool needed to get Feedback and Bug Reports",
+    siteName: "FeedbackThing",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "FeedbackThing",
+    description: "Your only tool needed to get Feedback and Bug Reports",
+    images: ["https://feedbackthing.pro/og.png"],
+    creator: "@eersnington",
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `https://feedbackthing.pro/site.webmanifest`,
 };
 
 export default function RootLayout({
