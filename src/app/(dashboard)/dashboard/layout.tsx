6dd0eb4cf39076509457783917689abb6898b9d7
Sree Narayanan <sreeaadhi07@gmail.com>
2024-09-22 05:56:31 +0400

    added fbt widget to dashboard

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { type ReactNode } from "react";
 import { type Metadata } from "next";
 import { Sidebar } from "../_components/sidebar";
 import { Header } from "../_components/header";
+import FeedbackThing from "@/components/feedbackthing";
 
 export const metadata: Metadata = {
   title: "Dashboard | FeedbackThing",
@@ -18,6 +19,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
         <div className="flex-1 overflow-auto bg-gray-50 p-6 dark:bg-gray-900">
           {children}
         </div>
+        <FeedbackThing projectId="d3baab1d-56fc-4742-acb7-aecbbc52ee0e" />
       </main>
     </div>
   );
